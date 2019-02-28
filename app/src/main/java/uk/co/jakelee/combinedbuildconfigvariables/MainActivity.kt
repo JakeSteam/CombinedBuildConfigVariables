c1cbec23aaae326ad79ff7e94082bf792689f607
Jake Lee <github@jakelee.co.uk>
2019-02-28 14:39:22 +0000

    Added example data, and layout

--- app/src/main/java/uk/co/jakelee/combinedbuildconfigvariables/MainActivity.kt
+++ app/src/main/java/uk/co/jakelee/combinedbuildconfigvariables/MainActivity.kt
@@ -3,12 +3,13 @@ package uk.co.jakelee.combinedbuildconfigvariables
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "The server's URL is: ${BuildConfig.SERVER_URL}!", Toast.LENGTH_LONG).show()
+        serverUrl.text = BuildConfig.SERVER_URL
     }
 }
